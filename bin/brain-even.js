b0329c504192d832553335e8bc7cfb5bb7617dc2
SergLyss <serge@expofp.com>
2023-01-17 00:33:00 +0300

    code review

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,31 +5,31 @@ function randomInteger() {
   return Math.floor(rand);
 }
 
+function isEven(num) {
+  return num % 2 === 0;
+}
+
 function startGame() {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
   for (let i = 0; i < 3; i++) {
     const number = randomInteger();
     console.log(`Question: ${number}`);
     const answer = readlineSync.question('Your answer: ');
-    if (number % 2 === 0) {
-      if (answer === 'yes') {
-        console.log('Correct!');
-      } else {
-        console.log(`'yes' is wrong answer ;(. Correct answer was 'yes'./n
-                Let's try again, ${userName}`);
-        return;
-      }
-    } else if (answer === 'no') {
+
+    if ((isEven(number) && answer === 'yes') || (!isEven(number) && answer === 'no')) {
       console.log('Correct!');
     } else {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'./n
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'no'./n
                 Let's try again, ${userName}`);
-      return;
     }
   }
+
   console.log(`Congratulations, ${userName}!`);
 }
 startGame();
